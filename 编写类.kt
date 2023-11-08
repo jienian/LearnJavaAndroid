1ec431fe02a62710fb682ce6e5880c348e46c3c5
jienian <2129300193@qq.com>
2023-11-08 18:38:30 +0800

    完成编写RecyclerAdapter

--- 编写类.kt
+++ 编写类.kt
@@ -20,3 +20,43 @@ implementation 'com.android.support:recyclerview-v7:28.0.0'
 //第三步，在Mainactivity.kt,使用旧的findViewByid()的方式：
 val forecastList = findViewById(R.id.forecast_list) as RecyclerView
 forecast_list.layoutManager = LinearLayoutManager(this)
+
+//第四步，The Recycler Adapter
+//RecyclerView中使用到的布局只需要一个TextView，简单创建文本列表，添加一个包名叫ForecastListAdapter.kt，代码如下
+class ForecastListAdapter(val items: List<String>) :
+    RectyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+        //函数创建ViewHolder
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+           // val view = LayoutInflater.from(parent.context).inflate(R.layout.forecast_list_item, parent, false)
+            return ViewHolder(TextView(parent.context))
+        }
+        //函数绑定数据
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.textView.text = items[position]
+        }
+        //函数返回数据列表的大小
+        override fun getItemCount(): Int {
+            return items.size
+        }
+
+    //内部类ViewHolder
+    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+}
+
+//第五步，回到MainActivity，创建String放入List中，然后使用创建分配Adapter如下。
+private val items = listOf(
+    "Today - Sunny - 88/63",
+    "Tomorrow - Foggy - 70/46",
+    "Weds - Cloudy - 72/63",
+    "Thurs - Rainy - 64/51",
+    "Fri - Foggy - 70/46",
+    "Sat - Cloudy - 72/63",
+)
+//第六步，在onCreate方法中使用Adapter
+override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    val forecastList = findViewById(R.id.forecast_list) as RecyclerView
+    forecastList.layoutManager = LinearLayoutManager(this)
+    forecastList.adapter = ForecastListAdapter(items)
+}
